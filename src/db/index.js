12e7d51c5542ebac37b1f46844f024bbb0a1e26a
HitanshSondhi <hitansh.sondhi.30@gmail.com>
2025-04-15 17:30:46 +0530

    databse connected successfully

--- src/db/index.js
+++ src/db/index.js
@@ -1,4 +1,28 @@
 import dotenv from "dotenv";
 import mongoose from "mongoose";
-import { DB_NAME } from "../../constants..js";
+import { DB_NAME } from "../../constants.js";
 
+dotenv.config();
+
+const connectDB=async ()=>{
+
+     try {
+    const connectionInstance = await mongoose.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("MongoDB connected ✅");
+    console.log(`DB Name: ${connectionInstance.connection.name}`);
+    console.log(`DB Host: ${connectionInstance.connection.host}`);
+    
+        
+    }catch(error){
+        console.log("Mongodb connection failed",error)
+
+        process.exit(1);
+
+    }
+};
+
+export default connectDB;
