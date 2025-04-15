2700c5a1374db95d0cd9ef464dfeb5e38c8e0fee
HitanshSondhi <hitansh.sondhi.30@gmail.com>
2025-04-15 17:43:23 +0530

    db connected

--- src/db/index.js
+++ src/db/index.js
@@ -7,14 +7,11 @@ dotenv.config();
 const connectDB=async ()=>{
 
      try {
-    const connectionInstance = await mongoose.connect(process.env.MONGODB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
-    console.log("MongoDB connected ✅");
-    console.log(`DB Name: ${connectionInstance.connection.name}`);
-    console.log(`DB Host: ${connectionInstance.connection.host}`);
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`, {
+           
+        });
+        console.log(`\n MongoDb connected !! Db Host ${connectionInstance.connection.host}`);
+        console.log(`Connection Status: ${connectionInstance.readyState}`);
     
         
     }catch(error){
